0dd4f31538b125849fc81c73e09696128e6e6bd6
fokhruli <fokhrul.rmedu@gmail.com>
2024-07-15 15:27:28 +0600

    bn_spbpe added

--- bn_spbpe.py
+++ bn_spbpe.py
@@ -13,8 +13,8 @@ class BnSpBpeTokenizer:
     def __init__(self):
         self.tokenizer = SentencePieceBPETokenizer(
             # vocab="/home/virus_proton/Projects/P_Projects/LLM_Mastery/Tokenizer_train/Trails/tokenizer_jul_13/test_vocab.json",
-            add_prefix_space=True,
             unk_token='<unk>',
+            add_prefix_space=True,
             replacement='_'
         )
 
@@ -29,7 +29,9 @@ class BnSpBpeTokenizer:
             files=files,
             vocab_size=vocab_size,
             min_frequency=min_frequency,
-            special_tokens=special_tokens
+            special_tokens=special_tokens,
+            limit_alphabet=700,
+            show_progress=True
         )
         
         # Add predefined words to the tokenizer vocabulary
@@ -76,31 +78,31 @@ if __name__=="__main__":
         "/storagex/Sabbir/BengaliTokenizer/characters.txt",
         "/storage2/llm_data/BanglaLM_process_v2.txt",
         '/storage2/llm_data/data_all_text/AllTextData/ai4bharat.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/BanglaLM_raw.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2012.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2013_20.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2013_48.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_15.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_23.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_35.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_41.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_42.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_49.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2014_52.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_06.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_11.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_14.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_18.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_22.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_27.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_32.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_35.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_40.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2015_48.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2016_30.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2016_50.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/bn.2017_17.txt',
-        '/storage2/llm_data/data_all_text/AllTextData/cc_100.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/BanglaLM_raw.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2012.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2013_20.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2013_48.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_15.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_23.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_35.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_41.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_42.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_49.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2014_52.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_06.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_11.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_14.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_18.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_22.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_27.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_32.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_35.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_40.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2015_48.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2016_30.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2016_50.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/bn.2017_17.txt',
+        # '/storage2/llm_data/data_all_text/AllTextData/cc_100.txt',
         ]
     _tokenizer = BnSpBpeTokenizer()
     hf_tokenizer = _tokenizer.train(
